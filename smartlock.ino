da20bcb00258fa14625e5e71285a15b7fd917f44
Kai Banks <kai@kais-mbp.home>
2018-04-02 03:09:12 -0500

    Removing XCode components and updating INO file to deal with stupid board issues

--- smartlock.ino
+++ smartlock.ino
@@ -1,17 +1,18 @@
 // This #include statement was automatically added by the Particle IDE.
 #include "Adafruit_PN532.h"
 
-const int SCK_PIN = A3;
-const int MOSI_PIN = A5;
+// pin variables & class scope variables
 const int SS_PIN = A2;
+const int SCK_PIN = A3;
 const int MISO_PIN = A4;
+const int MOSI_PIN = A5;
 
 const int IRQ_PIN = D2;
 const int RST_PIN = D3;
 
 const int LOCK_BUTTON = A0;
 
-String lockStatus;
+int lockStatus; // 1 is locked; 0 unlocked; 2 moving; -1 error
 
 bool authorized = false;
 
@@ -19,12 +20,14 @@ Servo motor;
 
 int servoPos = 0;
 
+// set to SPI or I2C depending on how the header file is configured
 #if PN532_MODE == PN532_SPI_MODE
   Adafruit_PN532 nfc(SCK_PIN, MISO_PIN, MOSI_PIN, SS_PIN);
 #elif PN532_MODE == PN532_I2C_MODE
   Adafruit_PN532 nfc(IRQ_PIN, RST_PIN);
 #endif
 
+// TODO: get this working to check the iPhone's supplied UID
 int testCredentials(String input) {
     if (input == "secret_key") {
         authorized = true;
@@ -35,27 +38,18 @@ int testCredentials(String input) {
     }
 }
 
+// runs once at the start of each boot-up
+// 1. get serial port up and running
+// 2. make sure the NFC board is recognized
+// 3. expose variables and functions to the Particle cloud
+// 4. get the servo set up
+// 5. get the unlock button set up
+// 6. set the status of the lock according to the servo's position
 void setup() {
-    Particle.function("toggleLock", toggleLock);
-    Particle.variable("lockStatus", lockStatus);
-    RGB.color(255, 255, 255);
-    motor.attach(WKP);
-
-    motor.write(180);
-    lockStatus = "unlocked";
-    Particle.function("testCreds", testCredentials);
-    pinMode(D7, OUTPUT);
-    pinMode(LOCK_BUTTON, INPUT_PULLUP);
+    // 1
     Serial.begin(115200);
 
-    setUpNFC();
-}
-
-bool isButtonPressed() {
-    return (digitalRead(LOCK_BUTTON) == 0);
-}
-
-void setUpNFC() {
+    // 2
     nfc.begin();
 
     uint32_t versiondata;
@@ -64,40 +58,58 @@ void setUpNFC() {
         versiondata = nfc.getFirmwareVersion();
         if (!versiondata) {
             Serial.println("no board");
-            Serial.println(SCK_PIN);
             delay(1000);
-
         }
     }
     while (!versiondata);
 
-    // Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX);
-    // Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC);
-    // Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
+    Serial.print("Found chip PN5");
+    Serial.println((versiondata>>24) & 0xFF, HEX);
+    Serial.print("Firmware ver. ");
+    Serial.print((versiondata>>16) & 0xFF, DEC);
+    Serial.print('.');
+    Serial.println((versiondata>>8) & 0xFF, DEC);
 
     // configure board to read RFID tags
     nfc.SAMConfig();
 
     Serial.println("Waiting for an ISO14443A Card ...");
+
+    // 3
+    Particle.function("toggleLock", toggleLock);
+    Particle.function("getStatus", getStatus);
+    Particle.variable("lockStatus", lockStatus);
+
+    // 4
+    motor.attach(D0);
+
+    // 5
+    pinMode(LOCK_BUTTON, INPUT_PULLUP);
+
+    // 6
+    lockStatus = getStatus("lock");
 }
 
-void loop() {
-    uint8_t success;
-    uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
-    uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+// check whether the hardware button is pressed
+bool isButtonPressed() {
+    return (digitalRead(LOCK_BUTTON) == 0);
+}
 
-    success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+// runs every 200ms to check for presence of NFC card
+void loop() {
+    delay(200);
+    Serial.print("lock status is: ");
+    Serial.println(lockStatus);
+    uint8_t success = 0;
+    uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+    uint8_t uidLength = 0;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+    success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
 
     if (success) {
         // Display some basic information about the card
         Serial.println("Found an ISO14443A card");
-        Serial.print("  UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
-        Serial.print("  UID Value: ");
-        nfc.PrintHex(uid, uidLength);
 
-        if (uidLength == 4)
-        {
-            // We probably have a Mifare Classic card ...
+        if (uidLength == 4) { // found a legit card; print info and unlock
             uint32_t cardid = uid[0];
             cardid <<= 8;
             cardid |= uid[1];
@@ -107,40 +119,60 @@ void loop() {
             cardid |= uid[3];
             Serial.print("Mifare Classic card #");
             Serial.println(cardid);
-            openLock();
-
+            unlock();
         }
 
         Serial.println("");
     }
-    else {
-        // motor.write(0);
-    }
 
-    if (isButtonPressed()) {
-        closeLock();
+    if (isButtonPressed() && lockStatus != 1) { // door is unlocked and the buttons was pressed; lock it
+        lock();
     }
 }
 
-int toggleLock (String input) {
-    if (lockStatus == "unlocked") {
-        closeLock();
+// cloud function to get current status of lock.
+// checks the servo directly for more accurate status.
+// returns the status
+int getStatus(String input) {
+    int current = motor.read();
+    if (current < 10) {
+        lockStatus = 1;
+    } else if (current > 175) {
+        lockStatus = 0;
     } else {
-        openLock();
+        lockStatus = 2;
     }
-    return 0;
+    return lockStatus;
+
 }
 
-void closeLock() {
-    digitalWrite(D7, LOW);
+// cloud function to toggle the state of the lock
+// returns the status
+int toggleLock(String input) {
+    if (lockStatus == 0) {
+        lock();
+    } else {
+        unlock();
+    }
+    return getStatus("lock");
+}
+
+// local function to lock the lock
+void lock() {
     motor.write(5);
     Serial.println("locking");
-    lockStatus = "locked";
+    lockStatus = 2;
+    delay(1000);
+    getStatus("lock");
+    nfc.SAMConfig();
 }
 
-void openLock() {
-    digitalWrite(D7, HIGH);
+// local function to lock the lock
+void unlock() {
     motor.write(180);
     Serial.println("unlocking");
-    lockStatus = "unlocked";
+    lockStatus = 2;
+    delay(1000);
+    getStatus("lock");
+    nfc.SAMConfig();
 }
